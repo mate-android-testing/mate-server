2ffed05e72b6b31d8bdd854ebcc0ee541aa6cc44
Andre Freire <and__re@hotmail.com>
2018-05-13 23:30:51 -0300

    exporting pdf

--- src/org/mate/Server2.java
+++ src/org/mate/Server2.java
@@ -1,6 +1,9 @@
 package org.mate;
 
-import com.sun.deploy.util.StringUtils;
+import com.itextpdf.text.*;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -32,7 +35,7 @@ public class Server2 {
         return "";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws DocumentException {
         isWin = false;
         String os = System.getProperty("os.name");
         if (os!=null && os.startsWith("Windows"))
@@ -87,6 +90,7 @@ public class Server2 {
                 Scanner cmd = new Scanner(client.getInputStream());
                 String cmdStr = cmd.nextLine();
 
+                System.out.println(cmdStr);
                 String response = "";
 
 
@@ -258,6 +262,44 @@ public class Server2 {
                 if (cmdStr.contains("randomlength"))
                     response = String.valueOf(length);
 
+                if(cmdStr.contains("FINISH")) {
+
+                    System.out.println(cmdStr);
+                    String name = cmdStr.split("_")[1];
+
+
+
+                    Document document = new Document();
+                    PdfWriter pdfWriter = PdfWriter.getInstance(document, new FileOutputStream(name + ".pdf"));
+
+                    pdfWriter.setStrictImageSequence(true);
+                    document.open();
+
+                    BufferedReader b = new BufferedReader(new FileReader(new File(name + ".txt")));
+                    String readLine = "";
+                    while ((readLine = b.readLine()) != null) {
+
+                        Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+                        Chunk chunk = new Chunk(readLine.split(",")[1] + ": " + readLine.split(",")[2], font);
+
+                        document.add(chunk);
+
+
+                        document.add(new Paragraph(""));
+
+                        Image img = Image.getInstance(readLine.split(",")[0]);
+                        img.scalePercent(25);
+                        document.add(img);
+                        document.add(new Paragraph(""));
+                        document.add(new Paragraph(""));
+                        document.add(new Paragraph(""));
+                    }
+
+
+                    document.close();
+
+
+                }
 
                 PrintWriter out = new PrintWriter(client.getOutputStream(), true);
                 out.println(response);
