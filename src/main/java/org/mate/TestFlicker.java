1e900465c05ef576823b8cb4aa6cf798f3590600
marceloeler <marceloeler@gmail.com>
2019-11-28 14:15:34 -0300

    new color features for widgets

---
+++ src/main/java/org/mate/TestFlicker.java
@@ -0,0 +1,32 @@
+package org.mate;
+
+public class TestFlicker {
+
+    public static void main(String[] args){
+        String screenShotDir = "/home/marceloeler/";
+        String targetFolder = screenShotDir;
+        System.out.println("target folder: " + targetFolder);
+
+        String emulator = "emulator-5554";
+        String imgPath = "screenshot-app.png";
+        String originalImgPath = imgPath;
+        int index = imgPath.lastIndexOf("_");
+        //String packageName = parts[1].substring(0,index-1);
+
+        String cmdStr = "";
+        for (int i=0; i<50; i++) {
+            imgPath = originalImgPath.replace(".png","_flicker_"+i+".png");
+            cmdStr = "adb -s " + emulator + " shell screencap -p /sdcard/" + imgPath;
+            //System.out.println(cmdStr);
+            ADB.runCommand(cmdStr);
+        }
+        for (int i=0; i<50; i++) {
+            imgPath = originalImgPath.replace(".png","_flicker_"+i+".png");
+            cmdStr = "adb -s " + emulator + " pull /sdcard/" + imgPath + " " + targetFolder;
+            //System.out.println(cmdStr);
+            ADB.runCommand(cmdStr);
+        }
+
+         boolean flickering = AccessibilityUtils.checkFlickering(targetFolder,originalImgPath);
+    }
+}
