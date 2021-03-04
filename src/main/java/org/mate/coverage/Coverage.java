4c29e83b94184f0c0cd5302fac752a56cb696083
Michael <auermich@fim.uni-passau.de>
2021-03-04 18:52:54 +0100

    fixed merge issue

--- src/main/java/org/mate/coverage/Coverage.java
+++ src/main/java/org/mate/coverage/Coverage.java
@@ -3,6 +3,7 @@ package org.mate.coverage;
 public enum Coverage {
     LINE_COVERAGE,
     BRANCH_COVERAGE,
+    METHOD_COVERAGE,
     BASIC_BLOCK_LINE_COVERAGE,
     BASIC_BLOCK_BRANCH_COVERAGE,
 }
