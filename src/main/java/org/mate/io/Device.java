d0b671dfb07be085ac1880d0ee86596dd2c05fd2
Michael Pusl <pusl03@ads.uni-passau.de>
2022-07-05 10:13:07 +0200

    Added: Get current Activity for API 30.

--- src/main/java/org/mate/io/Device.java
+++ src/main/java/org/mate/io/Device.java
@@ -496,7 +496,7 @@ public class Device {
             }
         }
 
-        if (getApiVersion() == 29) {
+        if (getApiVersion() == 29 || getApiVersion() == 30) {
             if (ProcessRunner.isWin) {
                 cmd = "$focused = " + androidEnvironment.getAdbExecutable() + " -s " + deviceID
                         + " shell dumpsys activity activities "
