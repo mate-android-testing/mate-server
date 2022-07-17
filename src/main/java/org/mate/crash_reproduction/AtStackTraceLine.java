97d7e332f295951d6f0dd0696e6f6ca58cef0c9b
Dominik Diner <diner@fim.uni-passau.de>
2022-07-17 11:47:12 +0200

    Moved logic into utility class

--- src/main/java/org/mate/crash_reproduction/AtStackTraceLine.java
+++ src/main/java/org/mate/crash_reproduction/AtStackTraceLine.java
@@ -62,14 +62,8 @@ public class AtStackTraceLine implements StackTraceLine {
                         Stream.of(methodName),
                         Arrays.stream(className.split("\\$"))
                 ))
-                .flatMap(this::splitCamelCase)
+                .flatMap(TokenUtil::splitCamelCase)
                 .map(String::toLowerCase);
-    }
-
-    private Stream<String> splitCamelCase(String camelCase) {
-        // Taken from https://stackoverflow.com/questions/7593969/regex-to-split-camelcase-or-titlecase-advanced/7594052#7594052
-        String[] array = camelCase.split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])");
-        return Arrays.stream(array);
     }
 
     @Override
