7870aac467aa934df70186ad9925f3b766d42b45
Michael <auermich@fim.uni-passau.de>
2023-04-25 13:36:07 +0200

    bump message protocol version to 3.3

--- src/main/java/org/mate/network/message/Messages.java
+++ src/main/java/org/mate/network/message/Messages.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class Messages {
     private static final String METADATA_PREFIX = "__meta__";
-    private static final String MESSAGE_PROTOCOL_VERSION = "3.2";
+    private static final String MESSAGE_PROTOCOL_VERSION = "3.3";
     private static final String MESSAGE_PROTOCOL_VERSION_KEY = "version";
 
     //util class
