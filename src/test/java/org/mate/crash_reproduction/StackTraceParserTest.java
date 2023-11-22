9ee7f26ebe56b4d5e61fb970627f8e486d2e86ec
Michael <auermich@fim.uni-passau.de>
2023-11-22 21:38:10 +0100

    added stack trace parser test

---
+++ src/test/java/org/mate/crash_reproduction/StackTraceParserTest.java
@@ -0,0 +1,27 @@
+package org.mate.crash_reproduction;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mate.util.Log;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
+
+public class StackTraceParserTest {
+
+    private static final File RESOURCES = new File("./src/test/java/resources/");
+
+    @BeforeAll
+    public static void setup() {
+        Log.registerLogger(new Log()); // Required for the logger invocations.
+    }
+
+    @Test
+    public void testStackTrace() {
+        final File stackTraceFile = new File(RESOURCES, "stack_trace_1.txt");
+        Assertions.assertDoesNotThrow(() -> StackTraceParser.parse(
+                Files.lines(stackTraceFile.toPath()).collect(Collectors.toList())));
+    }
+}
