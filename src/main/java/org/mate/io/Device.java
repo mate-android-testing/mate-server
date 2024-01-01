7999541972ff0654106de345184aec8527d4cd05
Michael <auermich@fim.uni-passau.de>
2024-01-01 22:38:29 +0100

    clean up

--- src/main/java/org/mate/io/Device.java
+++ src/main/java/org/mate/io/Device.java
@@ -9,7 +9,6 @@ import org.mate.util.Util;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
